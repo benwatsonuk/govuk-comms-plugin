c56e99bb46ce5fd6a0ae2a055bb09d2d7ec742e0
benwatsonuk <hello@benwatson.eu>
2023-01-20 16:49:18 +0000

    Beginning to convert to vanilla JS - but feel the methods make need rewriting with HTML injected rather than being required on page in the parent project

--- app/assets/all.js
+++ app/assets/all.js
@@ -4,18 +4,22 @@ function alertTheUser(message) {
   alert(message)
 }
 
+const theTrigger = document.getElementById('triggerDaysPassing')
+const theCounter = document.getElementById('daysPassing')
+
 function daysPass (page, waitTime, days) {
-  var theWaitTime = waitTime || 3000
+  console.log('Days passing function called')
+  const theWaitTime = waitTime || 3000
   setTimeout(function () {
-    $('#triggerDaysPassing').addClass('active')
+    theTrigger.classList.add('active')
   }, theWaitTime)
-  $('#triggerDaysPassing').on('click', function () {
-    $('#daysPassing').addClass('active')
-    $('#triggerDaysPassing').removeClass('active')
-    for (var i = 0; i <= days; ++i) {
+  theTrigger.addEventListener('click', function () {
+    theCounter.classList.add('active')
+    theTrigger.classList.remove('active')
+    for (let i = 0; i <= days; ++i) {
       setTimeout((function (x) {
         return function () {
-          $('#days').html(x)
+          document.getElementById('days').innerHTML = x.toString()
         }
       })(i), i * 1000)
     }
@@ -23,22 +27,22 @@ function daysPass (page, waitTime, days) {
   })
 }
 
-if ($('#daysPassing').length === 1) {
-  var url = ($('#daysPassing').attr('data-dayspassurl'))
-  var timeToPrompt = ($('#daysPassing').attr('data-dayspasstimetoprompt')) || 5000
-  var days = ($('#daysPassing').attr('data-dayspassdays')) || 5
+if (theCounter.length === 1) {
+  const url = (theCounter.getAttribute('data-dayspassurl'))
+  const timeToPrompt = (theCounter.getAttribute('data-dayspasstimetoprompt')) || 5000
+  const days = (theCounter.getAttribute('data-dayspassdays')) || 5
   daysPass(url, timeToPrompt, days)
 }
 
-if ($('#phoneBackground #phone-mockup').length === 1) {
-  $('#phone-mockup').on('click', function () {
-    $('#phoneBackground').toggleClass('active')
-    $(this).toggleClass('active')
-  })
-}
-
-if ($('#phoneAccessCode #phone-mockup').length === 1) {
-  setTimeout(function () {
-    $('#phone-mockup').toggleClass('active')
-  }, (3000))
-}
+// if ($('#phoneBackground #phone-mockup').length === 1) {
+//   $('#phone-mockup').on('click', function () {
+//     $('#phoneBackground').toggleClass('active')
+//     $(this).toggleClass('active')
+//   })
+// }
+//
+// if ($('#phoneAccessCode #phone-mockup').length === 1) {
+//   setTimeout(function () {
+//     $('#phone-mockup').toggleClass('active')
+//   }, (3000))
+// }
