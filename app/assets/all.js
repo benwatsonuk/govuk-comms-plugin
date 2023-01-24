81605f6cb879c90bfdc6c2145f1a516746bf678f
benwatsonuk <hello@benwatson.eu>
2023-01-24 10:20:04 +0000

    Appending to page

--- app/assets/all.js
+++ app/assets/all.js
@@ -19,7 +19,7 @@ const notifyUserOfMessage = (type, heading, message, from, target) => {
 
 const addHtmlToPage = (type, heading, message, from) => {
   console.log('addHtmlToPage')
-  document.getElementById('main-content').appendChild( setTheHtml(type))
+  document.getElementById('main-content').innerHTML += setTheHtml(type)
   addTheMessageToTheHtml(heading, message, from)
 }
 
@@ -42,32 +42,34 @@ const setTheHtml = (type) => {
   }
 }
 
-function daysPass (page, waitTime, days) {
-  console.log('Days passing function called')
-  const theWaitTime = waitTime || 3000
-  setTimeout(function () {
-    theTrigger.classList.add('active')
-  }, theWaitTime)
-  theTrigger.addEventListener('click', function () {
-    theCounter.classList.add('active')
-    theTrigger.classList.remove('active')
-    for (let i = 0; i <= days; ++i) {
-      setTimeout((function (x) {
-        return function () {
-          document.getElementById('days').innerHTML = x.toString()
-        }
-      })(i), i * 1000)
-    }
-    setTimeout(function () { document.location.href = page }, (days * 1000))
-  })
-}
+// The following could do with a rewrite
 
-if (theCounter.length === 1) {
-  const url = (theCounter.getAttribute('data-dayspassurl'))
-  const timeToPrompt = (theCounter.getAttribute('data-dayspasstimetoprompt')) || 5000
-  const days = (theCounter.getAttribute('data-dayspassdays')) || 5
-  daysPass(url, timeToPrompt, days)
-}
+// function daysPass (page, waitTime, days) {
+//   console.log('Days passing function called')
+//   const theWaitTime = waitTime || 3000
+//   setTimeout(function () {
+//     theTrigger.classList.add('active')
+//   }, theWaitTime)
+//   theTrigger.addEventListener('click', function () {
+//     theCounter.classList.add('active')
+//     theTrigger.classList.remove('active')
+//     for (let i = 0; i <= days; ++i) {
+//       setTimeout((function (x) {
+//         return function () {
+//           document.getElementById('days').innerHTML = x.toString()
+//         }
+//       })(i), i * 1000)
+//     }
+//     setTimeout(function () { document.location.href = page }, (days * 1000))
+//   })
+// }
+//
+// if (theCounter.length === 1) {
+//   const url = (theCounter.getAttribute('data-dayspassurl'))
+//   const timeToPrompt = (theCounter.getAttribute('data-dayspasstimetoprompt')) || 5000
+//   const days = (theCounter.getAttribute('data-dayspassdays')) || 5
+//   daysPass(url, timeToPrompt, days)
+// }
 
 // if ($('#phoneBackground #phone-mockup').length === 1) {
 //   $('#phone-mockup').on('click', function () {
